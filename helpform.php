945832d17630d5f6357e54d626cc1bb5f0e6d6e5
Oliver Kent <o.jkent@gmail.com>
2019-09-26 10:50:27 +0100

    Replaced use of depecated function

--- helpform.php
+++ helpform.php
@@ -15,12 +15,7 @@ echo $OUTPUT->header();
 $header_string = 'Helpform';
 
 if (isguestuser()) {
-	$wwwroot = $CFG->wwwroot .'/login/index.php';
-	if (!empty($CFG->loginhttps)) {
-		$wwwroot = str_replace('http:','https:', $wwwroot);
-	}
-
-	notice_yesno('The Help Form requires you to login.<br /><br />'.get_string('liketologin'), $wwwroot, $CFG->wwwroot);
+	echo $OUTPUT->notification('The Help Form requires you to login');
 				// $OUTPUT->confirm($message, $buttoncontinue, $buttoncancel).
 	echo $OUTPUT->footer();
 	die();
@@ -33,10 +28,10 @@ if (isset($_GET['link'])){
 }else{
 	$linker = '';
 }
-	
+
  // What page did they come from?
 $ref = $_SERVER['HTTP_REFERER'];
-$refturnitin = substr($ref,0,43);	
+$refturnitin = substr($ref,0,43);
 if (($linker == "turnitin")or ($linker == 'tutor')or($refturnitin == $CFG->wwwroot ."/mod/turnitintool/") or ($refturnitin == $CFG->wwwroot ."/mod/turnitintool")){
 	//Pick up some session variable sent from turnitin_view_amended.php
 	$assignmentname = $_SESSION['assignname'];
@@ -45,7 +40,7 @@ if (($linker == "turnitin")or ($linker == 'tutor')or($refturnitin == $CFG->wwwro
 	$tutorname = $_SESSION['ownername'];
 	$tutoremail = $_SESSION['owneremail'];
 }
-	
+
 echo '<style> #page-content{padding-top:20px;} .helpform{ width:100%; max-width: 800px; margin:auto;padding-right:10px; padding-left:10px;} textarea {width:95%;}</style>';
 echo '<div class="helpform">';
 // If the link from the help box is a turnitin.help request do this...
@@ -53,17 +48,17 @@ if ($linker == "turnitin") {
 	echo'	<div style="width: 100%; margin-right: auto; margin-left:auto; font-size: 13px; "><h4>Request Technical help with turnitin</h4>
 			</div>
 			<div style="margin-right: auto; margin-left:auto; font-size: 13px;  ">
-			Have you checked the <a  href="'. $CFG->wwwroot .'/turnitin-faq" target = "_blank"><strong>Frequently asked questions</strong></a> page for answers to your query?		
+			Have you checked the <a  href="'. $CFG->wwwroot .'/turnitin-faq" target = "_blank"><strong>Frequently asked questions</strong></a> page for answers to your query?
 			If you are having a technical problem with Turnitin please let us know.<br/>
 			<a href = '.$ref.'>...back to the assignment page</a><br/><br/>
 			Your Name: '.$USER->firstname.' '.$USER->lastname.'<br/>
 			Assignment name: '.$assignmentname.'<br/>
 			Unit Name:'.$coursename.'<br/>
-			Tutor Name:'.$tutorname.'<br/> <br/> 
+			Tutor Name:'.$tutorname.'<br/> <br/>
 			<form method="post" action="sendmail.php">
 				Message:<br />
 				<textarea cols="50" rows="10" name="message">
-				</textarea ><br /> 
+				</textarea ><br />
 				<input type="hidden" name="ref" value="'.$ref.'"/>
 				<input type="hidden" name="assignmentname" value="'.$assignmentname.'"/>
 				<input type="hidden" name="coursename" value="'.$coursename.'"/>
@@ -80,16 +75,16 @@ if ($linker == "turnitin") {
   	echo'	<div style="margin-right: auto; margin-left:auto; font-size: 13px; "><h4>Contact your tutor for assignment support</h4>
 			</div>
 			<div style="margin-right: auto; margin-left:auto; font-size: 13px;  ">
-			Have you checked the <a href="' . $CFG->dirroot . '/mod/resource/view.php?inpopup=true&id=175337" target = "_blank"><strong>Frequently asked questions</strong></a> page for answers to your query?<br/>		
+			Have you checked the <a href="' . $CFG->dirroot . '/mod/resource/view.php?inpopup=true&id=175337" target = "_blank"><strong>Frequently asked questions</strong></a> page for answers to your query?<br/>
 			<a href = '.$ref.'>...back to the assignment page</a><br/><br/>
 			Your Name: '.$USER->firstname.' '.$USER->lastname.'<br/>
 			Assignment name: '.$assignmentname.'<br/>
 			Unit Name:'.$coursename.'<br/>
-			Tutor Name:'.$tutorname.'<br/> <br/> 
+			Tutor Name:'.$tutorname.'<br/> <br/>
 			<form method="post" action="sendmail.php">
 				<strong>Message:</strong><br />
 				<textarea cols="50" rows="10" name="message">
-				</textarea><br /> 
+				</textarea><br />
 				<input type="hidden" name="ref" value="'.$ref.'"/>
 				<input type="hidden" name="assignmentname" value="'.$assignmentname.'"/>
 				<input type="hidden" name="coursename" value="'.$coursename.'"/>
@@ -106,7 +101,7 @@ if ($linker == "turnitin") {
 // If the request help link in the footer is clicked from a turnitin page do this...
 }elseif (($refturnitin == "http://learn.solent.ac.uk/mod/turnitintool/") or ($refturnitin == $CFG->wwwroot ."/mod/turnitintool")){
 	echo'	<div style=" margin-right: auto; margin-left:auto; font-size: 13px;"><h4>Request Technical help with turnitin</h4></div>
-			<div style="margin-right: auto; margin-left:auto; font-size: 13px;">		
+			<div style="margin-right: auto; margin-left:auto; font-size: 13px;">
 			Have you checked the <a  href="' . $CFG->dirroot . '/mod/resource/view.php?inpopup=true&id=175337" target = "_blank"><strong>Frequently asked questions</strong></a> page for answers to your query? If you are having a technical problem with Turnitin please let us know. <br/>
 			<a href = '.$ref.'>...back to the assignment page</a><br/><br/>
 			Your Name: '.$USER->firstname.' '.$USER->lastname.'<br/>
@@ -116,7 +111,7 @@ if ($linker == "turnitin") {
 			<form method="post" action="sendmail.php">
 				Message:<br />
 				<textarea cols="50" rows="10" name="message">
-				</textarea><br /> 
+				</textarea><br />
 				<input type="hidden" name="ref" value="'.$ref.'"/>
 				<input type="hidden" name="assignmentname" value="'.$assignmentname.'"/>
 				<input type="hidden" name="coursename" value="'.$coursename.'"/>
@@ -131,12 +126,12 @@ if ($linker == "turnitin") {
 //If it is any other page do this...
 }else{
 	echo'<div style=" margin-right: auto; margin-left:auto "><h4> Request Help Form</h4><br /></div>
-		<div style="margin-right: auto; margin-left:auto; font-size: 13pxem;  ">	
+		<div style="margin-right: auto; margin-left:auto; font-size: 13pxem;  ">
 			If you are having a problem with ' . $SITE->fullname . ' let us know.
 			We will receive an email with your details and try to respond within 1 working day.<br /><br />
 			<form method="post" action="sendmail.php">
 				Message:<br />
-				<textarea cols="50" rows="10" name="message"></textarea><br /> 
+				<textarea cols="50" rows="10" name="message"></textarea><br />
 				<input type="hidden" name="sendto" value="ltu@solent.ac.uk"/>
 				<input type="hidden" name="ref" value="'.$ref.'"/>
 				<input type="submit" Value="Request Help" />
@@ -144,8 +139,8 @@ if ($linker == "turnitin") {
 		</div>';
 }
 
-echo '</div>'; 
+echo '</div>';
 //put in your normal moodle footer:
 echo $OUTPUT->footer();
-    
+
 ?>
